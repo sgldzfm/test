902e047fb6d79f2edb648d808536c3e903db2ae7
sgldzfm <sgldzfm@sina.com>
2018-09-10 02:58:30 +0800

    完成了登陆的用户名密码的校验

--- admin/login.php
+++ admin/login.php
@@ -8,7 +8,36 @@
             $GLOBALS['message'] = '密码不能为空';
             return;
         endif;
-        //从文件中读取内容
+        //载入配置文件
+        require_once '../config.php';
+        //获取用户提交的数据
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        //使用md5加密，存在数据库里的密码也是md5加密过的
+        $password = md5($password);
+        //连接数据库
+        $conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        if(!$conn){
+            exit('<h1>数据库连接失败</h1>');
+        }
+        //查询用户提交过来的email值在数据库是否存在,当查询到一个时就停止往后查了，提高效率
+        $query = mysqli_query($conn,"select * from users where email = '{$email}' limit 1");
+        if(!$query){
+            $GLOBALS['message'] = '登陆失败，请重试';
+            return;
+        }
+        //从查询出来的结果中取第一条数据
+        $user = mysqli_fetch_assoc($query);
+        if(!$user){
+            $GLOBALS['message'] = '邮箱与密码不匹配';
+            return;
+        }
+        if($user['password'] !== $password){
+            $GLOBALS['message'] = '邮箱与密码不匹配';
+            return;
+        }
+        //校验成功，跳转到首页
+        header('Location: /admin/');
         /*$contents = file_get_contents("../uploads/user.txt");
         //拆分得到的字符串
         // $lines = explode("\n",$contents);
@@ -54,7 +83,7 @@
       <!-- 有错误信息时展示 -->
         <?php if(isset($message)):?>
             <div class="alert alert-danger bounce animated">
-                <strong>错误！</strong> 用户名或密码错误！
+                <strong><?php echo $message?></strong>
             </div>
         <?php endif;?>
 
