c6ad150a48cf08a13733b3a46a533e21da31cf2c
sgldzfm <sgldzfm@sina.com>
2018-09-11 20:20:26 +0800

    完成了对数据库查询的封装，和后台首页的所有动态数据的渲染

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,23 @@
     require_once '../functions.php';
     bx_get_current_user();
 
+    //使用数据库之前导入需要的常量
+    //载入配置文件
+    require_once '../config.php';
+    //查询数据库，获取文章总数
+    $posts_count = bx_fetch('select count(1) as num from posts');
+    //草稿总数:
+    $draft_count = bx_fetch("select count(1) as draft from posts where status = 'drafted'");
+    //分类总数:
+    $categories_count = bx_fetch('select count(1) as categories from categories');
+    //评论总数:
+    $comments_count = bx_fetch('select count(1) as comments from comments');
+    //待审核的评论总数:
+    $audit_count = bx_fetch("select count(1)  as audit from comments where status = 'held'");
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -42,9 +59,9 @@
               <h3 class="panel-title">站点内容统计：</h3>
             </div>
             <ul class="list-group">
-              <li class="list-group-item"><strong>10</strong>篇文章（<strong>2</strong>篇草稿）</li>
-              <li class="list-group-item"><strong>6</strong>个分类</li>
-              <li class="list-group-item"><strong>5</strong>条评论（<strong>1</strong>条待审核）</li>
+              <li class="list-group-item"><strong><?php echo $posts_count[0]['num']?></strong>篇文章（<strong><?php echo $draft_count[0]['draft']?></strong>篇草稿）</li>
+              <li class="list-group-item"><strong><?php echo $categories_count[0]['categories']?></strong>个分类</li>
+              <li class="list-group-item"><strong><?php echo $comments_count[0]['comments']?></strong>条评论（<strong><?php echo $audit_count[0]['audit']?></strong>条待审核）</li>
             </ul>
           </div>
         </div>
--- functions.php
+++ functions.php
@@ -12,3 +12,28 @@ function bx_get_current_user(){
     }
     return $_SESSION['current_login_user'];
 }
+
+//通过数据库查询获取数据
+//参数为sql语句
+function bx_fetch($sql){
+    //创建连接
+    $conn = mysqli_connect(BX_DB_HOST,BX_DB_USER,BX_DB_PASS,BX_DB_NAME);
+    //返回的连接为空说明创建连接失败
+    if(!$conn){
+        exit('连接失败');
+    }
+
+    $query = mysqli_query($conn,$sql);
+    //判断查询结果，如果为空说明查询失败
+    if(!$query){
+        return '查询失败';
+    }
+    //查询成功，就把值取出来。并放到一个数据里
+    //每次循环都会把一行数据放到变量$row里面。每一行都会是一个关联数组，是键值对的形式
+    while($row = mysqli_fetch_assoc($query)){
+        //然后把每次循环后的$row放到数组里，放到result里是一个线性数组，（就是线性数组里放关联数组的数据结构）
+        $result[] = $row;
+    }
+    //当循环完毕后，把数据作为返回值
+    return $result;
+}
